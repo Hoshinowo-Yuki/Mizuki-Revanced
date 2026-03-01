5148155ea7af13801c72aad170099cfaf54ed89a
shirakamiNeko <shirakamineko@lolicon.wtf>
2026-03-01 21:45:39 +0800

    chore: Refactor translations

--- src/config.ts
+++ src/config.ts
@@ -331,7 +331,7 @@ export const navBarConfig: NavBarConfig = {
 					icon: "material-symbols:group",
 				},
 				{
-					name: "Useful Links",
+					name: "usefulLinks",
 					url: "/links/",
 					icon: "mdi:web",
 				},
